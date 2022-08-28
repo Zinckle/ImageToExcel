2873e812ca279f8f952d330c371de5df8670dc33
Mitchell Zinck <58631706+Zinckle@users.noreply.github.com>
2022-08-28 13:14:35 -0400

    formating

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from openpyxl.styles import PatternFill, Color
 from openpyxl.utils import get_column_letter
 
 if __name__ == '__main__':
-    # imageName = input("Please enter an image name:")
-    imageName = "test.png"
+    imageName = input("Please enter an image name:")
     with Image.open(imageName) as inputImage:
         # inputImage.show()
         rgbInputImage = inputImage.convert('RGB')
@@ -21,17 +20,18 @@ if __name__ == '__main__':
         index = len(ws['A']) - shift
         for wPixel in range(1, width):
             r, g, b = rgbInputImage.getpixel((wPixel, hPixel))
-
             columnLetter = get_column_letter(wPixel)
+
             rVal = 'FF%02x%02x%02x' % (r, 0, 0)
             gVal = 'FF%02x%02x%02x' % (0, g, 0)
             bVal = 'FF%02x%02x%02x' % (0, 0, b)
+
             ws[columnLetter + str(index + hPixel)] = r
-            ws[columnLetter + str(index + hPixel)].fill = PatternFill(start_color=rVal, end_color=rVal, fill_type="solid")
+            ws[columnLetter + str(index + hPixel)].fill = PatternFill(start_color= rVal, end_color= rVal, fill_type = "solid")
             ws[columnLetter + str(index + hPixel + 1)] = g
-            ws[columnLetter + str(index + hPixel + 1)].fill = PatternFill(start_color=gVal, end_color=gVal, fill_type="solid")
+            ws[columnLetter + str(index + hPixel + 1)].fill = PatternFill(start_color= gVal, end_color= gVal, fill_type = "solid")
             ws[columnLetter + str(index + hPixel + 2)] = b
-            ws[columnLetter + str(index + hPixel + 2)].fill = PatternFill(start_color=bVal, end_color=bVal, fill_type="solid")
+            ws[columnLetter + str(index + hPixel + 2)].fill = PatternFill(start_color= bVal, end_color= bVal, fill_type = "solid")
 
     wb.save(filename='sample_book.xlsx')
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
