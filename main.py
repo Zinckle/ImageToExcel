66b3d6b51329c04b60aa9d880726e9b2033c14ae
Mitchell Zinck <58631706+Zinckle@users.noreply.github.com>
2022-09-23 20:17:54 -0400

    added ability to save all images to same work book and do multiple images at once

--- main.py
+++ main.py
@@ -1,51 +1,63 @@
+from time import sleep
+
 import openpyxl as openpyxl
 from PIL import Image
 from openpyxl.styles import PatternFill
 from openpyxl.utils import get_column_letter
-import gspread
+import webbrowser
+import os
 
 if __name__ == '__main__':
-    imageName = input("Please enter an image name:")
-    with Image.open(imageName) as inputImage:
-        # inputImage.show()
-        rgbInputImage = inputImage.convert('RGB')
-        width = inputImage.width
-        height = inputImage.height
+    # imageName = input("Please enter an image name:")
+    test = False
+    images = os.listdir('Images')
 
-    sa = gspread.service_account()
-    sh = sa.open("Image to Sheet")
-    worksheet = sh.add_worksheet(title="Image", rows=height, cols=width)
+    wb = openpyxl.Workbook()
+    counter = 0
 
-    wks = sh.worksheet("Image")
-    for hPixel in range(1, height):
-        shift = hPixel - 1 if hPixel > 1 else hPixel
+    for imageName in images:
+        print(imageName)
 
-        vals = [item for item in wks.col_values(1) if item]
-        index = len(vals)+1 - shift
+        wb.create_sheet(str(counter))
+        wb.active = wb[str(counter)]
+        ws = wb.active
+        ws.title = str(counter)
 
-        for wPixel in range(1, width):
-            r, g, b = rgbInputImage.getpixel((wPixel, hPixel))
-            columnLetter = get_column_letter(wPixel)
+        counter += 1
 
-            rVal = r/255
-            gVal = g/255
-            bVal = b/255
+        with Image.open('Images/' + imageName) as inputImage:
+            # inputImage.show()
+            rgbInputImage = inputImage.convert('RGB')
+            width = inputImage.width
+            height = inputImage.height
 
-            wks.format(columnLetter + str(index + hPixel), {
-                "backgroundColor": {
-                    "red": rVal,
-                    "green": 0.0,
-                    "blue": 0.0
-                }})
-            wks.format(columnLetter + str(index + hPixel + 1), {
-                "backgroundColor": {
-                    "red": 0.0,
-                    "green": gVal,
-                    "blue": 0.0
-                }})
-            wks.format(columnLetter + str(index + hPixel + 2), {
-                "backgroundColor": {
-                    "red": 0.0,
-                    "green": 0.0,
-                    "blue": bVal
-                }})
+        for hPixel in range(1, height):
+            shift = hPixel - 1 if hPixel > 1 else hPixel
+            index = len(ws['A']) - shift
+            for wPixel in range(1, width):
+                r, g, b = rgbInputImage.getpixel((wPixel, hPixel))
+                columnLetter = get_column_letter(wPixel)
+
+                rVal = 'FF%02x%02x%02x' % (r, 0, 0)
+                gVal = 'FF%02x%02x%02x' % (0, g, 0)
+                bVal = 'FF%02x%02x%02x' % (0, 0, b)
+
+                # ws[columnLetter + str(index + hPixel)] = r
+                ws[columnLetter + str(index + hPixel)].fill = PatternFill(start_color=rVal, end_color=rVal,
+                                                                          fill_type="solid")
+                # ws[columnLetter + str(index + hPixel + 1)] = g
+                ws[columnLetter + str(index + hPixel + 1)].fill = PatternFill(start_color=gVal, end_color=gVal,
+                                                                              fill_type="solid")
+                # ws[columnLetter + str(index + hPixel + 2)] = b
+                ws[columnLetter + str(index + hPixel + 2)].fill = PatternFill(start_color=bVal, end_color=bVal,
+                                                                              fill_type="solid")
+        ws.sheet_view.zoomScale = 5
+        ws.sheet_view.showGridLines = False
+        # if test:
+        # os.system("taskkill /im EXCEL.EXE /f")
+        # test = True
+    wb.remove(wb['Sheet'])
+    wb.save(filename='sample_book.xlsx')
+    webbrowser.open('sample_book.xlsx')
+    # sleep(1)
+    # os.system("taskkill /im EXCEL.EXE /f")
